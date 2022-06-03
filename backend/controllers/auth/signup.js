e470ace1eb3df7ed28d10863fcf7f8b84680fbf6
Disha Singh <disha9112@gmail.com>
2022-06-03 20:08:03 +0530

    removed auth comments

--- backend/controllers/auth/signup.js
+++ backend/controllers/auth/signup.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 const User = require("../../models/userModel");
 
 exports.signup = async (req, res) => {
-  //Extract data from request body
   const name = req.body.name;
   const password = req.body.password;
   const phoneNumber = req.body.phoneNumber;
@@ -15,7 +14,6 @@ exports.signup = async (req, res) => {
   const emailRegex =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  //Check if fields are not empty
   if (!name || !password || !phoneNumber || !email) {
     return res.status(400).send({
       status: false,
@@ -23,7 +21,6 @@ exports.signup = async (req, res) => {
     });
   }
 
-  //Check if the email already exists
   const emailExists = await User.exists({ email: email });
   if (emailExists) {
     return res.status(400).send({
@@ -46,10 +43,8 @@ exports.signup = async (req, res) => {
     });
   }
 
-  //Hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  //Add into Database
   const newUser = await User.create({
     name: name,
     password: hashedPassword,
@@ -57,10 +52,8 @@ exports.signup = async (req, res) => {
     email: email,
   });
 
-  //Generate JWT token
   const token = jwt.sign({ email: newUser.email }, process.env.JWT_TOKEN);
 
-  //Send the response
   return res.status(200).send({
     status: true,
     message: "User signed up successfully",
